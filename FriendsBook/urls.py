d1e0f6b1bbe6832fe2dd04d9cff2ca12cc3746d8
vishishtpriyadarshi <vishishtpriyadarshi867@gmail.com>
2019-01-24 01:42:30 +0530

    Minor Bugs Fixed - Week 2

--- FriendsBook/urls.py
+++ FriendsBook/urls.py
@@ -7,13 +7,13 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('update/', user_views.update, name='updates'),
-    path('', include('socialsite.urls')),
-    	]
+	path('admin/', admin.site.urls),
+	path('register/', user_views.register, name='register'),
+	path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+	path('update/', user_views.update, name='updates'),
+	path('', include('socialsite.urls')),
+		]
 
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -24,8 +24,20 @@ def register(request):
 
 @login_required
 def update(request):
-	u_form = UserUpdateForm()
-	p_form = ProfileUpdateForm()
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+		
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			messages.success(request,f'Your Account has been Updated!!')
+			return redirect('http://127.0.0.1:8000/profile')
+
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)
+	
 
 	context = {
 	'u_form': u_form,
